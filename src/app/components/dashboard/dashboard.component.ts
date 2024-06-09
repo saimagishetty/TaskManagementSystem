376493c6baf4491b3f1b2f7920cb0d863bce4f13
saicortracker <saikumar@cortracker.com>
2024-06-09 16:19:35 +0530

    update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -16,6 +16,7 @@ export class DashboardComponent {
 
   ngOnInit(): void {
     this.createPieChart();
+    this.createBarChart();
   }
 
   createPieChart() {
@@ -32,6 +33,32 @@ export class DashboardComponent {
           }]
         },
         options: {}
+      });
+    }
+  }
+  createBarChart() {
+    const ctx = document.getElementById('myBarChart') as HTMLCanvasElement;
+    if (ctx) {
+      const chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['To Do Tickets', 'Progress Tickets', 'Done Tickets', ],
+          datasets: [{
+            label: 'Tickets',
+            data: [69, 46, 93,],
+            backgroundColor: ['#5030E5','#FFA500','#8BC48A'],
+            borderColor: '#fff',
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              min: 10, // Set minimum value for the y-axis
+              max: 100 // Set maximum value for the y-axis
+            }
+          }
+          // Optional chart configuration options here
+        }
       });
     }
   }
